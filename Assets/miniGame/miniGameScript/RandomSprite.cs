6eaf7cfcd67cd6beb8b34f41ffaf39d66b813ceb
nelson915 <mynok2714@gmail.com>
2024-01-23 03:41:16 +0900

    inital main scene

--- Assets/miniGame/miniGameScript/RandomSprite.cs
+++ Assets/miniGame/miniGameScript/RandomSprite.cs
@@ -6,13 +6,11 @@ public class RandomSprite : MonoBehaviour
 {
     // 랜덤 장애물 생성 스크립트 ( 3.21.43 )    
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
