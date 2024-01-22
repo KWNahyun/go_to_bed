ff2581ed91a89200b3cad6a5bdf4de0bea22e423
nelson915 <mynok2714@gmail.com>
2024-01-23 00:58:32 +0900

    update : 고양이 애니매이션 수정

--- Assets/miniGame/miniGameScript/MiniGameControl.cs
+++ Assets/miniGame/miniGameScript/MiniGameControl.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class MiniGameControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum State { Idle, Run, Jump, Hit } 
+    public float StartJumpPower;
+    public float jumpPower;
+    public bool isGround;
+
+    //컴포넌트 불러오기 
+    Rigidbody2D rigid;
+    Animator anim;
+
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        StartJumpPower = 12.0f;
+        jumpPower = 1.0f;
+
+        anim.SetBool("isRun", true);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(Vector2.up * StartJumpPower, ForceMode2D.Impulse);
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+        anim.SetBool("isJump", false);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        anim.SetBool("isJump", true);
+        isGround = false;
+    }
+
+    // 해야할 거 
+    // 1.점프 ( 숏 점프와 롱 점프 )
+    // 2.착지
+    // 3.장애물 히트 
+    // 4.애니메이션 설정 
+    // 5.사운드 작업 
+
 }
