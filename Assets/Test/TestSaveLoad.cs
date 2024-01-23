9d95836813b01891ce26d1a09652f5255d3ee053
dbwofla11 <mynok2714@gmail.com>
2024-01-24 04:58:23 +0900

    add : StatDB추가 및 initialize 유틸추가
    initialize모듈은 "무조건" 한번 실행해야 StatDB가 로컬에 생성됨

--- Assets/Test/TestSaveLoad.cs
+++ Assets/Test/TestSaveLoad.cs
@@ -6,6 +6,7 @@ public class TestSaveLoad : MonoBehaviour
 {
     private void Update()
     {
+        // 세이브 예시 
         if (Input.GetKeyDown("s"))
         {
             SaveData stat = new SaveData("유재림", 10, 1, 1, 1);
@@ -13,6 +14,7 @@ public class TestSaveLoad : MonoBehaviour
             SaveSystem.Save(stat, "OnlyTest_001");
         }
 
+        // 로드 예시 
         if (Input.GetKeyDown("a"))
         {
             SaveData loadData = SaveSystem.Load("OnlyTest_001");
